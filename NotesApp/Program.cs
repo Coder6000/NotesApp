78920d9e48a6e9dff0798f8cd8125c54b98f5f31
Coder6000 <dummy-github-email>
2022-11-19 17:27:46 +0100

    First Commit

--- NotesApp/Program.cs
+++ NotesApp/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NotesApp;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        NoteApp noteApp = new NoteApp();
+
+        noteApp.Run();
+    }
+}
